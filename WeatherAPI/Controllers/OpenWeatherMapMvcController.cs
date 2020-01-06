c88f4263278de775d569f560274add73308544ab
Michael Royski <mroyski@gmail.com>
2020-01-06 17:01:56 -0500

    fill city function setup for index get

---
+++ WeatherAPI/Controllers/OpenWeatherMapMvcController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeatherAPI.Models;
+
+namespace WeatherAPI.Controllers
+{
+    public class OpenWeatherMapMvcController : Controller
+    {
+        public OpenWeatherMap FillCity()
+        {
+            OpenWeatherMap openWeatherMap = new OpenWeatherMap();
+            openWeatherMap.cities = new Dictionary<string, string>();
+            openWeatherMap.cities.Add("Melbourne", "7839805");
+            openWeatherMap.cities.Add("Auckland", "2193734");
+            openWeatherMap.cities.Add("New Delhi", "1261481");
+            openWeatherMap.cities.Add("Abu Dhabi", "292968");
+            openWeatherMap.cities.Add("Lahore", "1172451");
+            return openWeatherMap;
+        }
+
+        public IActionResult Index()
+        {
+            OpenWeatherMap openWeatherMap = FillCity();
+            return View(openWeatherMap);
+        }
+    }
+}
