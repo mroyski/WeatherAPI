9abd6b55081c696dedd780e98e81c9dee76df517
Michael Royski <mroyski@gmail.com>
2020-01-08 18:50:01 -0500

    created city search validation

---
+++ WeatherAPI/Models/SearchCity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherAPI.Models
+{
+    public class SearchCity
+    {
+        [Required(ErrorMessage = "You must enter a city name!")]
+        [RegularExpression("^[A-Za-z ]+$", ErrorMessage = "Only text allowed")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Enter a city name greater that 2 and lesser than 20 characters!")]
+        [Display(Name = "City Name")]
+        public string CityName { get; set; }
+    }
+}
