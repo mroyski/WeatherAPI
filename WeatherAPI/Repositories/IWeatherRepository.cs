f1e0937045b63967d90586fd24b157e542f890fe
Michael Royski <mroyski@gmail.com>
2020-01-08 19:15:37 -0500

    can deserialize json

---
+++ WeatherAPI/Repositories/IWeatherRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeatherAPI.Class;
+
+namespace WeatherAPI.Repositories
+{
+    public interface IWeatherRepository
+    {
+        WeatherResponse GetForecast(string city);
+    }
+}
---
+++ WeatherAPI/Repositories/WeatherRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualBasic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeatherAPI.Class;
+
+namespace WeatherAPI.Repositories
+{
+    public class WeatherRepository : IWeatherRepository
+    {
+        WeatherResponse IWeatherRepository.GetForecast(string city)
+        {
+            string IDOWeather = Constants.OPEN_WEATHER_APPID;
+            // Connection String
+            var client = new RestClient($"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&APPID={IDOWeather}");
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+
+            if (response.IsSuccessful)
+            {
+                // Deserialize the string content into JToken object
+                var content = JsonConvert.DeserializeObject<JToken>(response.Content);
+
+                // Deserialize the JToken object into our WeatherResponse Class
+                return content.ToObject<WeatherResponse>();
+            }
+
+            return null;
+        }
+    }
+}
